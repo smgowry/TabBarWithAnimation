bf0b776ab3f0b324339175032dd01ef0a1387e91
Gowrie <sm_gowry@yahoo.com>
2022-10-28 08:10:14 -0500

    tabbar with curve animation
    tabbar with curve animation

--- Sample/Sample/TabBarController.swift
+++ Sample/Sample/TabBarController.swift
@@ -7,55 +7,94 @@
 
 import UIKit
 
+enum TabBarItems {
+    case vw1
+    case vw2
+    case vw3
+    case vw4
+    case vw5
+    
+    var itemTitle: String {
+        switch self {
+        case .vw1:
+            return "vw1"
+        case .vw2:
+            return "vw2"
+        case .vw3:
+            return "vw3"
+        case .vw4:
+            return "vw4"
+        case .vw5:
+            return "vw5"
+        }
+    }
+    
+    var itemImage: String {
+        switch self {
+        case .vw1:
+            return "vw1"
+        case .vw2:
+            return "vw2"
+        case .vw3:
+            return "vw3"
+        case .vw4:
+            return "vw3"
+        case .vw5:
+            return "vw3"
+        }
+    }
+    
+    var controller: UIViewController? {
+        let storyboard = UIStoryboard(name: "Main" , bundle: nil)
+        let vw: UIViewController?
+        
+        switch self {
+        case .vw1:
+            vw = storyboard.instantiateViewController(withIdentifier: "ViewController1") as? ViewController1
+        case .vw2:
+            vw = storyboard.instantiateViewController(withIdentifier: "ViewController2") as? ViewController2
+        case .vw3:
+            vw = storyboard.instantiateViewController(withIdentifier: "ViewController3") as? ViewController3
+        case .vw4:
+            vw = storyboard.instantiateViewController(withIdentifier: "ViewController4") as? ViewController4
+        case .vw5:
+            vw = storyboard.instantiateViewController(withIdentifier: "ViewController5") as? ViewController5
+        }
+        
+        guard let viewcontroller = vw else {
+            return nil
+        }
+        viewcontroller.tabBarItem.title = itemTitle
+        viewcontroller.tabBarItem.image = UIImage(named: itemImage)
+        return viewcontroller
+    }
+}
 
 class TabBarController: UITabBarController {
+    var circleMask: UIView!
+    var tempLayers: [CAShapeLayer] = []
+    
     let shouldShowView3 = true
     var tabItems = [UIViewController]()
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         delegate = self
         addTabBarItems()
         tabBarTheme()
         
-        if tabItems.count > 0 {
-            for (i, item) in tabItems.enumerated() {
-                let itemView = tabBar.subviews[i]
-                if let imageView = itemView.subviews.first as? UIImageView {
-                    imageView.contentMode = .center
-                }
-            }
-        }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if let subView = tabBar.subviews[selectedIndex].subviews.first as? UIImageView {
-            animate(subView)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.addIndicatorView(index: self.selectedIndex, isFirstTime: true)
         }
     }
     
     private func addTabBarItems() {
-        
-        let storyboard = UIStoryboard(name: "Main" , bundle: nil)
-        
-        guard let vc1 = storyboard.instantiateViewController(withIdentifier: "ViewController1") as? ViewController1 else { return }
-        vc1.tabBarItem.title = "V1"
-        vc1.tabBarItem.image = UIImage(named: "vw1")
-        tabItems.append(vc1)
-        
-        guard let vc2 = storyboard.instantiateViewController(withIdentifier: "ViewController2") as? ViewController2 else { return }
-        vc2.tabBarItem.title = "V2"
-        vc2.tabBarItem.image = UIImage(named: "vw2")
-        tabItems.append(vc2)
-        
-        if shouldShowView3 {
-            guard let vc3 = storyboard.instantiateViewController(withIdentifier: "ViewController3") as? ViewController3 else { return }
-            vc3.tabBarItem.title = "V3"
-            vc3.tabBarItem.image = UIImage(named: "vw3")
-            tabItems.append(vc3)
+        let items: [TabBarItems] = [.vw1, .vw2, .vw3, .vw4, .vw5]
+        for (tabItem) in items {
+            if let controller = tabItem.controller {
+                tabItems.append(controller)
+            }
         }
-        
         viewControllers = tabItems
     }
     
@@ -67,42 +106,79 @@ class TabBarController: UITabBarController {
             tabBar.standardAppearance = appearance
             tabBar.scrollEdgeAppearance = tabBar.standardAppearance
         }
+        tabBar.tintColor = .white
+    }
+    
+    private func addIndicatorView(index: Int, isFirstTime: Bool = false) {
+        if !isFirstTime {
+            for temp in self.tempLayers {
+                temp.removeFromSuperlayer()
+            }
+            tempLayers.removeAll()
+            circleMask.removeFromSuperview()
+            self.view.layoutIfNeeded()
+        }
+        
+        for i in 0..<tabBar.items!.count {
+            guard let tabBarView = tabBar.items?[i].value(forKey: "view") as? UIView else {
+                return
+            }
+            
+            if let item = tabBar.items?[i] as? UITabBarItem {
+                item.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 0)
+            }
+            
+            let path: UIBezierPath = UIBezierPath()
+            path.move(to: CGPoint(x: tabBarView.frame.minX - 2,
+                                  y: 0.5))
+            
+            if i != index {
+                path.addLine(to: CGPoint(x: tabBarView.frame.maxX + 2,
+                                         y: 0.5))
+            } else {
+                let space: CGFloat = 30
+                let curveWidth = tabBarView.frame.size.width / 4
+                path.addCurve(to: CGPoint(x: tabBarView.frame.maxX + 2, y: 0.2),
+                              controlPoint1: CGPoint(x:  tabBarView.frame.minX + curveWidth, y: -space),
+                              controlPoint2: CGPoint(x: tabBarView.frame.minX + curveWidth * 3, y: -space))
+                
+                //Draw circle while animation
+                let width = tabBarView.frame.size.width/6 //8
+                let x: CGFloat = tabBarView.center.x - width / 2.0
+                circleMask = UIView(frame: CGRect(x: x,
+                                                  y: tabBarView.frame.minY + 15,
+                                                  width: width,
+                                                  height: width))
+                circleMask.backgroundColor = UIColor.red
+                circleMask.layer.cornerRadius = width / 2.0
+                circleMask.layer.masksToBounds = true
+                tabBar.insertSubview(circleMask, at: 0)
+                
+                if let item = tabBar.items?[i] as? UITabBarItem {
+                    item.titlePositionAdjustment = UIOffset(horizontal: 0,
+                                                            vertical: 20.0)
+                }
+            }
+            
+            let shapeLayer: CAShapeLayer = CAShapeLayer()
+            shapeLayer.path = path.cgPath
+            shapeLayer.fillColor = UIColor.clear.cgColor
+            shapeLayer.strokeColor = UIColor.red.cgColor
+            shapeLayer.borderWidth = 2.0
+            shapeLayer.fillMode = .forwards
+            tempLayers.append(shapeLayer)
+            tabBar.layer.addSublayer(shapeLayer)
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.1) {
+            self.circleMask.transform = CGAffineTransform(scaleX: 5, y: 5)
+        }
     }
 }
 
 
-
-extension TabBarController: UITabBarControllerDelegate  {
-    
-    private func animate(_ imageView: UIImageView) {
-
-        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .transitionCurlUp, animations: {
-            imageView.transform = CGAffineTransform(scaleX: 2.4, y: 2.4)
-        })
-    }
-    
-    private func resetAnimation(_ imageView: UIImageView) {
-        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 3.0, options: .curveEaseInOut, animations: {
-            imageView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-        }, completion: nil)
-
-    }
-
-    
-    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        
-        guard let index = self.tabBar.items?.firstIndex(of: item) else {
-            return
-        }
-    
-        if let subView = tabBar.subviews[index+1].subviews.first as? UIImageView {
-            self.animate(subView)
-        }
-        if index != self.selectedIndex,
-                  let subView = tabBar.subviews[self.selectedIndex+1].subviews.first as? UIImageView {
-            self.resetAnimation(subView)
-        }
-        
-        selectedIndex = index
+extension TabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        addIndicatorView(index: self.selectedIndex)
     }
 }
--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -7,34 +7,95 @@
 
 import UIKit
 
-class ViewController1: UIViewController {
+/*
+class BaseViewController: UIViewController {
+    var index: Int = 0
+    var pathLayer: CAShapeLayer?
+    var displayLink: CADisplayLink?
 
-    @IBOutlet weak var labeltitle: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        labeltitle.text = "One"
-        self.view.backgroundColor = .systemGray
+        
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        displayLink = CADisplayLink(target: self, selector: #selector(renderTopLine))
+        displayLink?.add(to: RunLoop.current, forMode: .default)
+    }
+    
+    @objc func renderTopLine() {
+        if pathLayer != nil {
+            pathLayer?.removeFromSuperlayer()
+        }
+        
+        index+=1
+        print(index)
+        let path: UIBezierPath = UIBezierPath()
+        path.move(to:CGPoint(x: 0, y: 300))
+        let space: CGFloat = 100.0 + CGFloat(index * 3)
+        path.addCurve(to: CGPoint(x: view.bounds.size.width/2, y: 300),
+                      controlPoint1: CGPoint(x: view.bounds.size.width / 8, y: 300 - space),
+                      controlPoint2: CGPoint(x: view.bounds.size.width / 2 / 4 * 3, y: 300 - space))
+        
+        pathLayer = CAShapeLayer()
+        pathLayer?.frame = self.view.bounds
+        pathLayer?.path = path.cgPath
+        pathLayer?.strokeColor = UIColor.purple.cgColor
+        pathLayer?.fillColor = nil
+        pathLayer?.lineWidth = 2.0
+        pathLayer?.lineJoin = CAShapeLayerLineJoin.bevel
+        //view.layer.addSublayer(pathLayer!)
+        view.layoutIfNeeded()
+        
+        if index == 3 {
+            displayLink?.invalidate()
+            displayLink = nil
+            index = 0
+        }
+    }
 }
+   */
+class ViewController1: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
+
 
 class ViewController2: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemGray
+        self.view.backgroundColor = .white
     }
-
-
 }
 
 class ViewController3: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemGray
+        self.view.backgroundColor = .white
+    }
+}
+
+
+
+class ViewController4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
+
+
+class ViewController5: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
     }
 
 
